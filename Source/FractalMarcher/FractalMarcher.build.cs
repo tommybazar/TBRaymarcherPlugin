87b27c22e54b50864a40753cb254ee6484bcaa6a
tommybazar <tombaza@seznam.czr>
2022-03-19 15:32:06 +0800

    commented out proceduralMeshActor, cleaned up build.cs in fractalMarcher

--- Source/FractalMarcher/FractalMarcher.build.cs
+++ Source/FractalMarcher/FractalMarcher.build.cs
@@ -2,62 +2,34 @@
 
 using UnrealBuildTool;
 
-public class FractalMarcher: ModuleRules
+public class FractalMarcher : ModuleRules
 {
 	public FractalMarcher(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-        CppStandard = CppStandardVersion.Cpp17;
+		CppStandard = CppStandardVersion.Cpp17;
 
-        PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-			
-		
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
-                "Core",
-                "RenderCore",
-                "Engine",
-                "RHI",
-                "AssetRegistry",
-                "VolumeTextureToolkit",
-				"HeadMountedDisplay",
-				"InputCore",
-				"ProceduralMeshComponent"
-				// ... add other public dependencies that you statically link with here ...
+				"Core",
+				"RenderCore",
+				"Engine",
+				"RHI",
+				"AssetRegistry",
+				"VolumeTextureToolkit",
+				"InputCore"
 			}
-            );
-			
-		
+		);
+
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
 				"CoreUObject",
-				"Engine",
 				"Slate",
-                "SlateCore",
-                "UMG",
-				// ... add private dependencies that you statically link with here ...	
+				"SlateCore",
 			}
-            );
-		
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
+		);
 	}
 }
