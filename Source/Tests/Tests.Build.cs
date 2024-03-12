c79eaf4f284633c93cef62a4333e7cca6016a721
SolityForFree <strax950@gmail.com>
2024-03-12 16:19:01 +0100

    Added Tests module. UE is unable to load them.

---
+++ Source/Tests/Tests.Build.cs
@@ -0,0 +1,27 @@
+﻿using UnrealBuildTool;
+
+public class Tests : ModuleRules
+{
+    public Tests(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core"
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "FunctionalTesting"
+            }
+        );
+    }
+}
