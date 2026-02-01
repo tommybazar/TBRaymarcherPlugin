978cb0c3b3d78066a5a5be9ed65a62a457206cfa
Ugur Taysi <utaysi@gmail.com>
2026-02-01 17:35:12 +0300

    feat(build): add linux platform support for dcmtk libraries
    Add platform-conditional build configuration for VolumeTextureToolkit. Windows continues using bundled dcmtk DLLs and libs while Linux uses system-installed dcmtk libraries via PublicSystemLibraries.

--- Source/VolumeTextureToolkit/VolumeTextureToolkit.Build.cs
+++ Source/VolumeTextureToolkit/VolumeTextureToolkit.Build.cs
@@ -35,33 +35,51 @@ public class VolumeTextureToolkit : ModuleRules
 			}
 		);
 
-		string BinPath = System.IO.Path.Combine(ModuleDirectory, "ThirdParty/dcmtk", "bin", Target.Platform.ToString());
-		string LibPath = System.IO.Path.Combine(ModuleDirectory, "ThirdParty/dcmtk", "lib", Target.Platform.ToString());
-		string IncludePath = System.IO.Path.Combine(ModuleDirectory, "ThirdParty/dcmtk", "include");
-
-		string[] DLLs = {
-			"oficonv.dll", "ofstd.dll", "oflog.dll", "dcmdata.dll", "ijg8.dll", "ijg12.dll", "ijg16.dll",
-			"dcmimgle.dll", "dcmimage.dll", "dcmjpeg.dll", "dcmtkcharls.dll", "dcmjpls.dll"
-		};
-		string[] Libs =
+		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
-			"oficonv.lib", "ofstd.lib", "oflog.lib", "dcmdata.lib", "ijg8.lib", "ijg12.lib", "ijg16.lib",
-			"dcmimgle.lib", "dcmimage.lib", "dcmjpeg.lib", "dcmtkcharls.lib", "dcmjpls.lib"
-		};
+			string BinPath = System.IO.Path.Combine(ModuleDirectory, "ThirdParty/dcmtk", "bin", Target.Platform.ToString());
+			string LibPath = System.IO.Path.Combine(ModuleDirectory, "ThirdParty/dcmtk", "lib", Target.Platform.ToString());
+			string IncludePath = System.IO.Path.Combine(ModuleDirectory, "ThirdParty/dcmtk", "include");
 
-		PrivateIncludePaths.Add(IncludePath);
+			string[] DLLs = {
+				"oficonv.dll", "ofstd.dll", "oflog.dll", "dcmdata.dll", "ijg8.dll", "ijg12.dll", "ijg16.dll",
+				"dcmimgle.dll", "dcmimage.dll", "dcmjpeg.dll", "dcmtkcharls.dll", "dcmjpls.dll"
+			};
+			string[] Libs =
+			{
+				"oficonv.lib", "ofstd.lib", "oflog.lib", "dcmdata.lib", "ijg8.lib", "ijg12.lib", "ijg16.lib",
+				"dcmimgle.lib", "dcmimage.lib", "dcmjpeg.lib", "dcmtkcharls.lib", "dcmjpls.lib"
+			};
 
-		foreach (string DLL in DLLs)
-		{
-			string FullPath = System.IO.Path.Combine(BinPath, DLL);
-			PublicDelayLoadDLLs.Add(DLL);
-			RuntimeDependencies.Add(FullPath);
+			PrivateIncludePaths.Add(IncludePath);
+
+			foreach (string DLL in DLLs)
+			{
+				string FullPath = System.IO.Path.Combine(BinPath, DLL);
+				PublicDelayLoadDLLs.Add(DLL);
+				RuntimeDependencies.Add(FullPath);
+			}
+
+			foreach (string Lib in Libs)
+			{
+				string FullPath = System.IO.Path.Combine(LibPath, Lib);
+				PublicAdditionalLibraries.Add(FullPath);
+			}
 		}
-
-		foreach (string Lib in Libs)
+		else if (Target.Platform == UnrealTargetPlatform.Linux)
 		{
-			string FullPath = System.IO.Path.Combine(LibPath, Lib);
-			PublicAdditionalLibraries.Add(FullPath);
+			// Use system-installed dcmtk on Linux
+			PublicSystemIncludePaths.Add("/usr/include");
+
+			string[] LinuxLibs = {
+				"ofstd", "oflog", "oficonv", "dcmdata", "dcmimgle", "dcmimage",
+				"dcmjpeg", "ijg8", "ijg12", "ijg16", "dcmjpls", "dcmtkcharls"
+			};
+
+			foreach (string Lib in LinuxLibs)
+			{
+				PublicSystemLibraries.Add(Lib);
+			}
 		}
 	}
 }
